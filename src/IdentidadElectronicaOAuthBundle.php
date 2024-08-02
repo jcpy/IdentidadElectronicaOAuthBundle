4736f5450490e00470d4b059cda0d448450e3b00
Daniel Insaurralde <60437018+danielinsaurralde@users.noreply.github.com>
2024-08-02 15:55:07 -0400

    Add services

--- src/IdentidadElectronicaOAuthBundle.php
+++ src/IdentidadElectronicaOAuthBundle.php
@@ -2,8 +2,13 @@
 
 namespace Mitic\IdentidadElectronicaOAuthBundle;
 
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class IdentidadElectronicaOAuthBundle extends Bundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
+    }
 }
