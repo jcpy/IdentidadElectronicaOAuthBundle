02309205008bf03f47d01035088d17dac8ab45af
Daniel Insaurralde <60437018+danielinsaurralde@users.noreply.github.com>
2024-08-06 15:07:31 -0400

    Update IdentidadElectronicaOAuthBundle.php

--- src/IdentidadElectronicaOAuthBundle.php
+++ src/IdentidadElectronicaOAuthBundle.php
@@ -3,9 +3,10 @@
 namespace Mitic\IdentidadElectronicaOAuthBundle;
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class IdentidadElectronicaOAuthBundle extends Bundle
+class IdentidadElectronicaOAuthBundle extends AbstractBundle
 {
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
